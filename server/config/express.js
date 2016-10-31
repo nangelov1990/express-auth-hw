42cf618ab96fd6368b44c7c5cf1511afe3751a88
nangelov1990 <nangelov1990@gmail.com>
2016-10-31 11:43:02 +0200

    express config

--- server/config/express.js
+++ server/config/express.js
@@ -1,2 +1,9 @@
 'use strict'
+const express = require('express')
 
+module.exports = (app, config) => {
+  app.set('view engine', 'pug')
+  app.set('views', config.rootPath + 'server/views')
+
+  app.use(express.static(config.rootPath + 'public'))
+}
